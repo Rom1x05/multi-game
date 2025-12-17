e6a50e6eede172173448adaefd76d77566bb800b
Rom1x05 <romcle05@gmail.com>
2025-12-17 10:23:24 +0100

    ajout config

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { GameProvider } from './context/GameContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <GameProvider>
         <App />
       </GameProvider>
